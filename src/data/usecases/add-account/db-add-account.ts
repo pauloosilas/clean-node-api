617a3709a2385392617086ad00a7ed13c4398f7f
pauloosilas <paulo3356@gmail.com>
2023-06-23 16:12:27 -0300

    test: ensure DbAddAccount throws if AddAccountRepository throws

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -11,6 +11,11 @@ export class DbAddAccount implements AddAccount {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const hashedPassword = await this.encrypter.encrypt(accountData.password)
     await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashedPassword }))
-    return await new Promise(resolve => resolve(null))
+    return await new Promise(resolve => resolve({
+      id: '',
+      name: '',
+      email: '',
+      password: ''
+    }))
   }
 }
